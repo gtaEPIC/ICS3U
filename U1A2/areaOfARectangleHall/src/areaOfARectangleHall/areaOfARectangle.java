d5b17d01d1507efd0d48a20b46bdbebbc16df7f4
Johnny <johnnygotrain@gmail.com>
2020-02-19 17:41:28 -0500

    New Assingment
    Created a new assignment under Intellij

---
+++ U1A2/areaOfARectangleHall/src/areaOfARectangleHall/areaOfARectangle.java
@@ -0,0 +1,13 @@
+package areaOfARectangleHall;
+
+import java.util.Scanner;
+
+
+public class areaOfARectangle {
+
+    public static void main(String[] args) {
+        Scanner keyedInput = new.Scanner(System.in);
+
+    }
+
+}
