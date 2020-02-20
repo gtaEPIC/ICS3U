a094b0b9d32eb2c392b8946b053ca15cf177acfb
Johnny <johnnygotrain@gmail.com>
2020-02-19 22:28:19 -0500

    Created Many More Applications
    Created more applications to finish some assignments

--- U1A2/areaOfARectangleHall/src/areaOfARectangleHall/areaOfARectangle.java
+++ U1A2/areaOfARectangleHall/src/areaOfARectangleHall/areaOfARectangle.java
@@ -6,7 +6,18 @@ import java.util.Scanner;
 public class areaOfARectangle {
 
     public static void main(String[] args) {
-        Scanner keyedInput = new.Scanner(System.in);
+        Scanner keyedInput = new Scanner(System.in);
+
+        double length;
+        double width;
+
+        System.out.println("Hello! Please enter the length of a square you want to find the area for (Does not have to be a whole number):");
+        length = keyedInput.nextDouble();
+        System.out.println("Finally, please enter the width of the same square:");
+        width = keyedInput.nextDouble();
+        System.out.println("Perfect! Now I just go to calculate your math problem");
+        double area = length * width;
+        System.out.println("Ta da! All finished! The area of " + length + " x " + width + " is: " + area);
 
     }
 
