6d075d291b7ea7c9b9bf1b24e408501abc08ed3c
Johnny <johnnygotrain@gmail.com>
2020-02-27 09:51:52 -0500

    Fixed broken Merge
    Merge deleted other files that weren't mean to be deleted.

---
+++ U1A1/src/aboutMeHall/aboutMeHall.java
@@ -0,0 +1,22 @@
+package aboutMeHall;
+
+
+public class aboutMeHall {
+
+    public static void main(String[] args) {
+        System.out.println("1.What is your name:");
+        System.out.println("Johnny Hall");
+        System.out.println("2.Where do you live:");
+        System.out.println("Bowmanville, Ontatio, Canada");
+        System.out.println("3.Why did you decide to enroll in this course:");
+        System.out.println("I decided to enroll in this course because I love to program and want to learn something new.");
+        System.out.println("4.What do you hope to learn about in this course:");
+        System.out.println("I hope to learn how to use/program in java.");
+        System.out.println("5.What is your favourite piece of technology:");
+        System.out.println("My favourite piece of technology I would say is a robot.");
+        System.out.println("6. What is your favourite app or computer program:");
+        System.out.println("My favourite app is openBVE");
+        System.out.println("7. What is one thing interesting/funny about you:");
+        System.out.println("One thing interesting about me is that I really love trains");
+    }
+}
---
+++ U1A2/TypesofVariablesHall/src/typesofVariablesHall/typesofVariablesHall.java
@@ -0,0 +1,22 @@
+package typesofVariablesHall;
+
+public class typesofVariablesHall {
+    public static void main(String[] args) {
+        int intVar;
+        double doubleVar;
+        String stringVar;
+        boolean boolVar;
+
+        intVar = -17;
+        doubleVar = 13.6;
+        stringVar = "Hello, World!";
+        boolVar = true;
+
+        System.out.println("Value of intVar: " + intVar);
+        System.out.println("Value of doubleVar: " + doubleVar);
+        System.out.println("Value of stringVar: " + stringVar);
+        System.out.println("Value of boolVar: " + boolVar);
+
+
+    }
+}
