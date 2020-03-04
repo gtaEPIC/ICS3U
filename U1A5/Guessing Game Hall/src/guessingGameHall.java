34b7d1d4b7112209ef07cdbef92dd252118717f4
Johnny <johnnygotrain@gmail.com>
2020-03-04 12:55:16 -0500

    Started Making New Program
    Created new unit, and a new program (In progress) called guessing game.

---
+++ U1A5/Guessing Game Hall/src/guessingGameHall.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class guessingGameHall {
+    private JTextField txtGuess;
+    private JButton btnRoll;
+    private JLabel lblTitle;
+    private JLabel lblGuess;
+    private JLabel lblRoll;
+    private JLabel lblRolled;
+    private JLabel lblInfoRoll;
+    private JLabel lblRolls;
+    private JLabel lblInfoCorrectGuess;
+    private JLabel lblInfoWrongGuess;
+    private JLabel lblInfoPercent;
+    private JLabel lblCorrectGuess;
+    private JLabel lblWrongGuess;
+    private JLabel lblPercent;
+    private JLabel lblInfo;
+}
